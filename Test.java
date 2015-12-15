2eebdeac61b32e43dee716d1f1c77e1113d5ed1f
CayeCaye <stefan.buttenhauser@gmx.net>
2015-12-15 20:24:05 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -6,6 +6,7 @@
 public class Test {
 	public static void main(String[] args){
 		/** Test 1 */
+		System.out.println("--- Test 1 ---");
 		SchachtelLager sl = new SchachtelLager();
 		for (int i = 1; i < 20; i++){
 			sl.addSchachtel(new Schachtel(i, new Rechteck(i,i,i)));
@@ -49,5 +50,49 @@ public class Test {
 		et.inhalt();
 
 		/** Test 2 */
+		System.out.println("--- Test 2 ---");
+
+		Einkaufstasche et2 = new Einkaufstasche();
+		for (int i = 1; i < 3; i++){
+			sl.addSchachtel(new Schachtel(i, new Rechteck(i,i,i)));
+			sl.addSchachtel(new Schachtel(i, new Kreis(i,i)));
+			sl.addSchachtel(new Schachtel(i, new Kreis(i,i)));
+			sl.addSchachtel(new Schachtel(i, new Kreis(i,i)));
+		}
+		for(int i = 1; i < 3; i++){
+			Schachtel h = null;
+			Geschenk g1 = new Geschenk(new Rechteck(i,i,i), "Rechteck"+i);
+			Geschenk g2 = new Geschenk(new Quadrat(i,i), "Quadrat"+i);
+			Geschenk g3 = new Geschenk(new Kreis(i,i), "Kreis"+i);
+			Geschenk g4 = new Geschenk(new Sechseck(i,i), "Sechseck"+i);
+			h=g1.verpacke(sl);
+			if(h == null){
+				et2.addGeschenk(g1);
+			}else{
+				et2.addGeschenk(h);
+			}
+			h=g2.verpacke(sl);
+			if(h == null){
+				et2.addGeschenk(g2);
+			}else{
+				et2.addGeschenk(h);
+			}
+			h=g3.verpacke(sl);
+			if(h == null){
+				et2.addGeschenk(g3);
+			}else{
+				et2.addGeschenk(h);
+			}
+			h=g4.verpacke(sl);
+			if(h == null){
+				et2.addGeschenk(g4);
+			}else{
+				et2.addGeschenk(h);
+			}
+		}
+
+		System.out.println("Einkaufstasche Volumen: " + et.volumen());
+		et2.inhalt();
+
 	}
 }
